e30f00e1de6bc1169c980623babc9f9eb9dd0c11
Wei Lee <leewei48750@gmail.com>
2020-02-29 23:52:14 +0800

    feat(webpack.base.js): feat css-loader config

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -1,13 +1,13 @@
 // webpack.base.js
-'use strict';
-const path = require('path');
+"use strict";
+const path = require("path");
 
 module.exports = {
-  context: path.resolve(__dirname, '../'),
-  entry: path.join(__dirname, '../', 'src', 'index.tsx'),
+  context: path.resolve(__dirname, "../"),
+  entry: path.join(__dirname, "../", "src", "index.tsx"),
   output: {
-    path: path.resolve(__dirname, '../', 'dist'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "../", "dist"),
+    filename: "bundle.js"
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js"]
@@ -27,14 +27,26 @@ module.exports = {
         enforce: "pre",
         test: /\.js$/,
         loader: "source-map-loader"
+      },
+      {
+        test: /\.css/i,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
       }
     ]
   },
   node: {
     setImmediate: false,
-    fs: 'empty',
-    net: 'empty',
-    tls: 'empty',
-    child_process: 'empty'
-  },
+    fs: "empty",
+    net: "empty",
+    tls: "empty",
+    child_process: "empty"
+  }
 };
